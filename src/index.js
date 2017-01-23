fc8a1e3ff2881964031e5d4d064b4f7583a08cc1
Andrew Ochtinsky <aochtinsky@quadanalytix.com>
2017-01-23 13:26:53 -0800

    dist/index.html is no longer a source file. this file is now found in src/static/index.html and is included via a require() statement in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App/App';
 
+require('file?name=[name].[ext]!./static/index.html');
+
+
+
 const render = (Component) => {
   ReactDOM.render(
     <div>
